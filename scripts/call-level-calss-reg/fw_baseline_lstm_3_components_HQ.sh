eb5f154d16ab84baf0910130c6546cbb91a4ba8e
Ge Li <ge.li@mila.quebec>
2023-08-28 16:31:57 -0400

    update script

--- scripts/call-level-calss-reg/fw_baseline_lstm_3_components_HQ.sh
+++ scripts/call-level-calss-reg/fw_baseline_lstm_3_components_HQ.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=baselines_lstm_call_level_class_reg_fwc_3CH_HQ
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
-#SBATCH --gres gpu:1
+#SBATCH --gres gpu:rtx8000:1
 #SBATCH --partition=long
 #SBATCH --output=/network/projects/aia//whale_call/LOGS/%x-%j.out
 #SBATCH --error=/network/projects/aia//whale_call/LOGS/%x-%j.err
@@ -19,7 +19,8 @@ RUN_NAME="HQ"
 python models/lstm.py \
     --data-path /network/projects/aia/whale_call/LABELS/FWC_3CH_HQ \
     --save-dir /network/projects/aia/whale_call/wandb_log/$PROJECT/$EXP_NAME \
-    --project $PROJECT \    --exp-name $EXP_NAME \
+    --project $PROJECT \
+    --exp-name $EXP_NAME \
     --run-name $RUN_NAME \
     --input-dim 129 \
     --hidden-dim 128 \
